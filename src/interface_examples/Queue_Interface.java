713086b1f9e9fb0f9d19525793e15c9b8f44107f
raje2411 <rraman@hortonworks.com>
2018-09-11 05:45:14 -0400

    Queue Interface

---
+++ src/interface_examples/Queue_Interface.java
@@ -0,0 +1,13 @@
+//This interface is to create a queue for characters with arrays
+//Following 3 types of implementation 
+//1. Queue with fixed size
+//2. linear queue
+//3. Circular queue
+package interface_examples;
+
+public interface Queue_Interface {
+	
+	public char get();
+	public void put(char input);
+	
+}
